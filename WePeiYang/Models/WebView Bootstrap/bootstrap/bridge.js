ea0fb13ef0ba7fb52cfe26ec88420de4b4c08e02
Qin Yubo <xnth97@live.com>
2016-02-06 22:48:53 +0800

    fix question header

--- WePeiYang/Models/WebView Bootstrap/bootstrap/bridge.js
+++ WePeiYang/Models/WebView Bootstrap/bootstrap/bridge.js
@@ -1,3 +1,7 @@
+/*
+    WebViewJavaScriptBridge
+*/
+
 function setupWebViewJavascriptBridge(callback) {
     if (window.WebViewJavascriptBridge) { return callback(WebViewJavascriptBridge); }
     if (window.WVJBCallbacks) { return window.WVJBCallbacks.push(callback); }
@@ -15,7 +19,7 @@ setupWebViewJavascriptBridge(function(bridge) {
     $(".img-responsive").each(function() {
         $(this).unbind().bind('click', function() {
             bridge.callHandler('imgCallback', $(this).attr('src'), function(response) {
-                
+                console.log('JS received: ', response);
             });
         });
     });
