ff050087d92ab6ba0b9a3a942d61e9a260945920
twtstudio <jhxssq@gmail.com>
2016-08-23 16:45:17 +0800

    WePeiYang 2.0.0 Release Version

--- WePeiYang/Homepage/MainViewController.swift
+++ WePeiYang/Homepage/MainViewController.swift
@@ -300,7 +300,7 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         case 0:
             self.showGPAController()
         case 1:
-            self.showLibraryController()
+            self.showBicycleServiceController()
         case 2:
             self.showClasstableController()
         case 3:
@@ -395,6 +395,47 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         microserviceController.presentInViewController(self)
     }
 
+    
+    func showBicycleServiceController() {
+        let bikeVC = BicycleServiceViewController()
+        //log.word(NSUserDefaults.standardUserDefaults().objectForKey("twtToken") as! String)/
+        
+        if NSUserDefaults.standardUserDefaults().objectForKey("twtToken") == nil {
+            let loginVC = LoginViewController(nibName: nil, bundle: nil)
+            self.presentViewController(loginVC, animated: true, completion: nil)
+        } else {
+            //坑：让后面能自动弹出要求绑定，但是做法不太科学，应改为 Notification
+            BicycleUser.sharedInstance.bindCancel = false
+            /*
+             if NSUserDefaults.standardUserDefaults().valueForKey("BicycleToken") != nil {
+             
+             let BicycleExpire = NSUserDefaults.standardUserDefaults().valueForKey("BicycleExpire") as? Int
+             let now = NSDate()
+             let timeInterval: NSTimeInterval = now.timeIntervalSince1970
+             let timeStamp = Int(timeInterval)
+             if timeStamp <= BicycleExpire {
+             //隐藏tabbar
+             bikeVC.hidesBottomBarWhenPushed = true;
+             self.navigationController?.showViewController(bikeVC, sender: nil)
+             } else {
+             BicycleUser.sharedInstance.auth({
+             //隐藏tabbar
+             bikeVC.hidesBottomBarWhenPushed = true;
+             self.navigationController?.showViewController(bikeVC, sender: nil)
+             })
+             }
+             
+             } else {
+             }*/
+            BicycleUser.sharedInstance.auth({
+                //隐藏tabbar
+                bikeVC.hidesBottomBarWhenPushed = true;
+                self.navigationController?.showViewController(bikeVC, sender: nil)
+            })
+        }
+        
+        
+    }
     /*
     // MARK: - Navigation
 
--- WePeiYang/Homepage/View/HomeToolsCell.swift
+++ WePeiYang/Homepage/View/HomeToolsCell.swift
@@ -20,7 +20,7 @@ class HomeToolsCell: UITableViewCell, UICollectionViewDataSource, UICollectionVi
     
     let homeTools = [
         (title: "成绩", image: UIImage(named: "gpaBtn")!),
-        (title: "图书馆", image: UIImage(named: "libBtn")!),
+        (title: "自行车", image: UIImage(named: "bicycleBtn")!),
         (title: "课程表", image: UIImage(named: "classtableBtn")!),
 //        (title: "失物招领", image: UIImage(named: "lfBtn")!)
         (title: "实验室", image: UIImage(named: "msBtn")!)
