34c5cea9a11f392ffce086cfc6727dba1a2af976
allenx <mint.mr@gmail.com>
2016-11-01 20:53:29 +0800

    Merging Everyone's work together

--- WePeiYang/Read/View/CoverView.swift
+++ WePeiYang/Read/View/CoverView.swift
@@ -25,6 +25,8 @@ class CoverView: UIView {
     var starReviewLabel: UILabel!
     var summaryLabel: UILabel!
     
+    var summaryTitleLabel: UILabel!
+    var tapToSeeMoreBtn: UIButton!
     
     convenience init(book: Book) {
         self.init()
@@ -66,6 +68,12 @@ class CoverView: UIView {
             return $0
         }(UILabel(text: "出版时间："+book.year, color: .grayColor()))
         
+        //Gotta refine it
+        scoreLabel = {
+            $0.font = UIFont(name: "Menlo-BoldItalic", size: 60)
+            return $0
+        }(UILabel(text: "\(book.rating)", color: .yellowColor()))
+        
         favBtn = UIButton(title: "收藏", borderWidth: 1.5, borderColor: readRed)
         favBtn.addTarget(self, action: #selector(self.favourite), forControlEvents: .TouchUpInside)
         reviewBtn = UIButton(title: "写书评", borderWidth: 1.5, borderColor: readRed)
@@ -78,23 +86,54 @@ class CoverView: UIView {
             return $0
         }(UILabel(text: "This is a very great book", color: .grayColor()))
         
+        summaryTitleLabel = {
+            $0.font = UIFont.systemFontOfSize(12)
+            return $0
+        }(UILabel(text: "图书简介", color: .grayColor()))
+        
         summaryLabel = {
-            //TODO: DO OTHER UI MODIFICATIONS
-            $0.numberOfLines = 0
+            //TODO: DO OTHER UI MODIFICATIONS & Click to read more
+            $0.numberOfLines = 4
             $0.font = UIFont.systemFontOfSize(15)
             return $0
         }(UILabel(text: book.summary, color: .grayColor()))
         
-        let img = UIImage(named: "cover4")
-        if img?.size.height > img?.size.width {
-            coverView.image = UIImage.resizedImageKeepingRatio(img!, scaledToWidth: UIScreen.mainScreen().bounds.width / 2)
-        } else {
-            coverView.image = UIImage.resizedImageKeepingRatio(img!, scaledToHeight: UIScreen.mainScreen().bounds.height * 0.52 * 0.6)
+        tapToSeeMoreBtn = {
+            $0.addTarget(self, action: #selector(self.tapToSeeMore), forControlEvents: .TouchUpInside)
+            $0.titleLabel?.textColor = readRed
+            return $0
+        }(UIButton(title: "点击查看更多"))
+        
+        //let img = UIImage(named: "cover4")
+        
+        let imageURL = NSURL(string: book.coverURL)
+        let imageRequest = NSURLRequest(URL: imageURL!)
+        
+        coverView.setImageWithURLRequest(imageRequest, placeholderImage: nil, success: { (_, _, img) in
+            if img.size.height > img.size.width {
+                self.coverView.image = UIImage.resizedImageKeepingRatio(img, scaledToWidth: UIScreen.mainScreen().bounds.width / 2)
+            } else {
+                self.coverView.image = UIImage.resizedImageKeepingRatio(img, scaledToHeight: UIScreen.mainScreen().bounds.height * 0.52 * 0.6)
+            }
+            
+            let fooRGB = self.coverView.image?.smartAvgRGB()
+            self.computedBGView.backgroundColor = UIColor(red: (fooRGB?.red)!, green: (fooRGB?.green)!, blue: (fooRGB?.blue)!, alpha: (fooRGB?.alpha)!)
+        }) { (_, _, _) in
+            guard let img = UIImage(named: "placeHolderImageForBookCover") else {
+                self.coverView.backgroundColor = .grayColor()
+                let fooRGB = self.coverView.image?.smartAvgRGB()
+                self.computedBGView.backgroundColor = UIColor(red: (fooRGB?.red)!, green: (fooRGB?.green)!, blue: (fooRGB?.blue)!, alpha: (fooRGB?.alpha)!)
+                return
+            }
+            self.coverView.image = img
+            let fooRGB = self.coverView.image?.smartAvgRGB()
+            self.computedBGView.backgroundColor = UIColor(red: (fooRGB?.red)!, green: (fooRGB?.green)!, blue: (fooRGB?.blue)!, alpha: (fooRGB?.alpha)!)
         }
         
         
-        let fooRGB = coverView.image?.smartAvgRGB()
-        computedBGView.backgroundColor = UIColor(red: (fooRGB?.red)!, green: (fooRGB?.green)!, blue: (fooRGB?.blue)!, alpha: (fooRGB?.alpha)!)
+        //Can't do it outside the Asynchronous Closure
+        //        let fooRGB = coverView.image?.smartAvgRGB()
+        //        computedBGView.backgroundColor = UIColor(red: (fooRGB?.red)!, green: (fooRGB?.green)!, blue: (fooRGB?.blue)!, alpha: (fooRGB?.alpha)!)
         
         computedBGView.addSubview(coverView)
         coverView.snp_makeConstraints {
@@ -158,11 +197,25 @@ class CoverView: UIView {
             make.height.equalTo(36)
         }
         
+        self.addSubview(scoreLabel)
+        scoreLabel.snp_makeConstraints {
+            make in
+            make.centerY.equalTo(authorLabel)
+            make.centerX.equalTo(reviewBtn)
+        }
+        
+        self.addSubview(summaryTitleLabel)
+        summaryTitleLabel.snp_makeConstraints {
+            make in
+            make.top.equalTo(favBtn.snp_bottom).offset(28)
+            make.left.equalTo(favBtn)
+        }
+        
         self.addSubview(summaryLabel)
         summaryLabel.snp_makeConstraints {
             make in
-            make.top.equalTo(favBtn.snp_bottom).offset(50)
-            make.left.equalTo(favBtn)
+            make.top.equalTo(summaryTitleLabel.snp_bottom).offset(20)
+            make.left.equalTo(summaryTitleLabel)
             make.right.equalTo(reviewBtn)
         }
         
@@ -183,11 +236,11 @@ private extension UIButton {
         titleLabel?.font = UIFont.boldSystemFontOfSize(18)
         //titleLabel?.sizeToFit()
         tintColor = borderColor
-    
+        
         
         //TODO: self-sizing UIButton with Snapkit
-//        frame.size = CGSize(width: ((titleLabel?.frame.width)! + 60), height: ((titleLabel?.frame.height)! + 20))
-//        print((titleLabel?.frame.width)! + 50)
+        //        frame.size = CGSize(width: ((titleLabel?.frame.width)! + 60), height: ((titleLabel?.frame.height)! + 20))
+        //        print((titleLabel?.frame.width)! + 50)
         //let titleSize = NSString(string: title).sizeWithAttributes(<#T##attrs: [String : AnyObject]?##[String : AnyObject]?#>)
         //sizeThatFits(CGSize(width: ((titleLabel?.frame.width)! + 50), height: ((titleLabel?.frame.height)! + 20)))
         self.layer.cornerRadius = 3
@@ -197,22 +250,22 @@ private extension UIButton {
 }
 
 
-extension CoverView {
+extension CoverView: UIWebViewDelegate {
     func presentRateView() {
         let rateView = RateView(rating: 3.0)
         //self.addSubview(rateView)
         
         
-//        UIViewController.currentViewController().view.addSubview(rateView)
-//        rateView.snp_makeConstraints {
-//            make in
-//            make.height.equalTo(UIScreen.mainScreen().bounds.height)
-//            make.width.equalTo(UIScreen.mainScreen().bounds.width)
-//            make.top.equalTo(UIViewController.currentViewController().view)
-//            make.left.equalTo(UIViewController.currentViewController().view)
-//        }
+        //        UIViewController.currentViewController().view.addSubview(rateView)
+        //        rateView.snp_makeConstraints {
+        //            make in
+        //            make.height.equalTo(UIScreen.mainScreen().bounds.height)
+        //            make.width.equalTo(UIScreen.mainScreen().bounds.width)
+        //            make.top.equalTo(UIViewController.currentViewController().view)
+        //            make.left.equalTo(UIViewController.currentViewController().view)
+        //        }
         
-//        UIViewController.currentViewController().navigationview.addSubview(rateView)
+        //        UIViewController.currentViewController().navigationview.addSubview(rateView)
         UIViewController.currentViewController().navigationController?.view.addSubview(rateView)
         rateView.frame = CGRect(x: 0, y: self.frame.height, width: 0, height: self.frame.height/4)
         UIView.beginAnimations("ratingViewPopUp", context: nil)
@@ -224,15 +277,15 @@ extension CoverView {
     
     func assignGestures(to whichView: UIView) {
         whichView.userInteractionEnabled = true
-//        
-//        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(self.expandView))
-//        pinchGestureRecognizer.scale = 1.2
-//        whichView.addGestureRecognizer(pinchGestureRecognizer)
-//        
-//        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(self.dismissAnimatedAndRemoveDefaults))
-//        swipeUp.direction = .Up
-//        
-//        whichView.addGestureRecognizer(swipeUp)
+        //
+        //        let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(self.expandView))
+        //        pinchGestureRecognizer.scale = 1.2
+        //        whichView.addGestureRecognizer(pinchGestureRecognizer)
+        //
+        //        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(self.dismissAnimatedAndRemoveDefaults))
+        //        swipeUp.direction = .Up
+        //
+        //        whichView.addGestureRecognizer(swipeUp)
         
     }
     
@@ -240,5 +293,32 @@ extension CoverView {
         //Call `favourite` method of a user
     }
     
+    func tapToSeeMore() {
+        let summaryDetailView = UIWebView()
+        summaryDetailView.delegate = self
+        summaryDetailView.userInteractionEnabled = true
+        summaryDetailView.loadHTMLString(book.summary, baseURL: nil)
+        UIViewController.currentViewController().navigationController?.view.addSubview(summaryDetailView)
+        summaryDetailView.frame = CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+        summaryDetailView.alpha = 0
+        UIView.beginAnimations("summaryDetailViewFadeIn", context: nil)
+        UIView.setAnimationDuration(0.6)
+        summaryDetailView.alpha = 1
+        UIView.commitAnimations()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.tapAgainToDismiss))
+        summaryDetailView.addGestureRecognizer(tap)
+    }
     
+    func tapAgainToDismiss() {
+        for fooView in (UIViewController.currentViewController().navigationController?.view.subviews)! {
+            if fooView.isKindOfClass(UIWebView) {
+                UIView.beginAnimations("summaryDetailViewFadeIn", context: nil)
+                UIView.setAnimationDuration(0.6)
+                fooView.alpha = 0
+                fooView.removeFromSuperview()
+                UIView.commitAnimations()
+            }
+        }
+    }
 }
--- WePeiYang/Read/View/ReviewCell.swift
+++ WePeiYang/Read/View/ReviewCell.swift
@@ -14,7 +14,7 @@ class ReviewCell: UITableViewCell {
     var avatar: UIImageView = UIImageView()
     var content: UILabel = UILabel()
     var username: UILabel = UILabel()
-    var rateView: RateStarView = RateStarView()
+    var starView: StarView!
     var timestamp: UILabel = UILabel()
     var heartView: UIImageView = UIImageView()
     var like: UILabel = UILabel()
@@ -34,7 +34,9 @@ class ReviewCell: UITableViewCell {
         self.like.text = model.like
         // TODO: TimeStamp
         self.timestamp.text = model.updateTime
-        self.rateView.count = Int(model.rating)
+        
+        //Determine the look of the starView
+        self.starView = StarView(rating: model.rating, height: 17, tappable: false)
         // TODO: 用户名
         self.username.text = model.userName
         
@@ -51,10 +53,10 @@ class ReviewCell: UITableViewCell {
         self.contentView.addSubview(avatar)
         self.contentView.addSubview(content)
         self.contentView.addSubview(username)
-        self.contentView.addSubview(rateView)
+        self.contentView.addSubview(starView)
         self.contentView.addSubview(timestamp)
-//        self.contentView.addSubview(heartView)
-//        self.contentView.addSubview(like)
+        //        self.contentView.addSubview(heartView)
+        //        self.contentView.addSubview(like)
         self.contentView.addSubview(separator)
         self.contentView.addSubview(fooView)
         
@@ -68,7 +70,7 @@ class ReviewCell: UITableViewCell {
             make.height.equalTo(kAVATAR_HEIGHT)
             make.width.equalTo(kAVATAR_HEIGHT)
         }
-
+        
         avatar.layer.cornerRadius = CGFloat(kAVATAR_HEIGHT/2)
         avatar.layer.masksToBounds = true
         
@@ -80,9 +82,9 @@ class ReviewCell: UITableViewCell {
             make.top.equalTo(contentView).offset(15)
         }
         
-        rateView.snp_makeConstraints { make in
-          //  make.width.equalTo(120)
-          //  make.height.equalTo(20)
+        starView.snp_makeConstraints { make in
+            //  make.width.equalTo(120)
+            //  make.height.equalTo(20)
             make.top.equalTo(username.snp_bottom).offset(3)
             make.left.equalTo(avatar.snp_right).offset(10)
         }
@@ -106,7 +108,7 @@ class ReviewCell: UITableViewCell {
             make.top.equalTo(avatar.snp_bottom).offset(10)
             make.right.equalTo(contentView).offset(-20)
         }
-                
+        
         timestamp.textColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
         if #available(iOS 8.2, *) {
             timestamp.font = UIFont.systemFontOfSize(14, weight: UIFontWeightLight)
@@ -135,19 +137,19 @@ class ReviewCell: UITableViewCell {
             make.height.equalTo(30)
         }
         
-//        like.userInteractionEnabled = true
-//        let tap1 = UITapGestureRecognizer(target: self, action: #selector(self.likeTapped))
-//        like.addGestureRecognizer(tap1)
-//        heartView.userInteractionEnabled = true
-//        let tap2 = UITapGestureRecognizer(target: self, action: #selector(self.likeTapped))
-//        heartView.addGestureRecognizer(tap2)
+        //        like.userInteractionEnabled = true
+        //        let tap1 = UITapGestureRecognizer(target: self, action: #selector(self.likeTapped))
+        //        like.addGestureRecognizer(tap1)
+        //        heartView.userInteractionEnabled = true
+        //        let tap2 = UITapGestureRecognizer(target: self, action: #selector(self.likeTapped))
+        //        heartView.addGestureRecognizer(tap2)
         fooView.userInteractionEnabled = true
         fooView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.likeTapped)))
         like.textColor = UIColor(red: 0.6, green: 0.6, blue: 0.6, alpha: 1)
         like.snp_makeConstraints { make in
-//            make.top.equalTo(content.snp_bottom).offset(4)
-//            make.right.equalTo(contentView).offset(-30)
-//            make.bottom.equalTo(contentView).offset(-20)
+            //            make.top.equalTo(content.snp_bottom).offset(4)
+            //            make.right.equalTo(contentView).offset(-30)
+            //            make.bottom.equalTo(contentView).offset(-20)
             make.top.equalTo(fooView).offset(8)
             make.right.equalTo(fooView).offset(-8)
             
@@ -156,7 +158,7 @@ class ReviewCell: UITableViewCell {
         heartView.image = UIImage(named: "grey_heart")
         heartView.snp_makeConstraints { make in
             make.right.equalTo(like.snp_left).offset(-3)
-           // make.top.equalTo(contentView).offset(16)
+            // make.top.equalTo(contentView).offset(16)
             make.centerY.equalTo(like.snp_centerY)
             //make.height.equalTo(14)
             //make.width.equalTo(15)
@@ -173,29 +175,29 @@ class ReviewCell: UITableViewCell {
             make.bottom.equalTo(contentView).offset(0)
         }
         
-
+        
     }
     
     func likeTapped() {
         if self.heartView.tag == 0 {
-        self.like.tag += 1
-        let frame = self.heartView.frame
-        let width = frame.size.width
-        let height = frame.size.height
-        self.heartView.frame = CGRect(x: self.heartView.frame.origin.x - width/2 , y: self.heartView.frame.origin.y - height/2, width: self.heartView.frame.size.width*2, height: self.heartView.frame.size.height*2)
-        UIView.animateWithDuration(0.25, animations: {
-            self.heartView.image = UIImage(named: "red_heart")
-            self.like.text = String(format: "%02d", self.like.tag)
-            self.heartView.frame = frame
-            self.heartView.tag = 1
-        })
+            self.like.tag += 1
+            let frame = self.heartView.frame
+            let width = frame.size.width
+            let height = frame.size.height
+            self.heartView.frame = CGRect(x: self.heartView.frame.origin.x - width/2 , y: self.heartView.frame.origin.y - height/2, width: self.heartView.frame.size.width*2, height: self.heartView.frame.size.height*2)
+            UIView.animateWithDuration(0.25, animations: {
+                self.heartView.image = UIImage(named: "red_heart")
+                self.like.text = String(format: "%02d", self.like.tag)
+                self.heartView.frame = frame
+                self.heartView.tag = 1
+            })
         } else {
             self.like.tag -= 1
             self.like.text = String(format: "%02d", self.like.tag)
             self.heartView.image = UIImage(named: "grey_heart")
             self.heartView.tag = 0
         }
-
+        
     }
     
     func attributedString(title: String, content: String) -> NSMutableAttributedString {
@@ -207,55 +209,55 @@ class ReviewCell: UITableViewCell {
         
         return mutableAttributedString
     }
-
+    
     
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
 
-class RateStarView: UIView {
-    var imgArr: [UIImageView] = []
-    var count = 3{
-        didSet{
-            refreshView()
-        }
-    }
-    
-    override func drawRect(rect: CGRect) {
-        super.drawRect(rect)
-        refreshView()
-    }
-    
-    func refreshView() {
-        let width = 17
-        let height = 15
-        if imgArr.count == 0 {
-            for i in 0..<count{
-                let imgView = UIImageView(image: UIImage(named: "red_star"))
-              imgView.frame = CGRectMake(CGFloat(i*width), 0, CGFloat(width), CGFloat(height))
-                self.imgArr.append(imgView)
-                self.addSubview(imgView)
-            }
-            for i in count..<5 {
-                let imgView = UIImageView(image: UIImage(named: "grey_star"))
-                imgView.frame = CGRectMake(CGFloat(i*width), 0, CGFloat(width), CGFloat(height))
-                self.imgArr.append(imgView)
-                self.addSubview(imgView)
-            }
-        }
-        else{
-            for i in 0..<count{
-                imgArr[i].image = UIImage(named: "red_star")
-            }
-            for i in count..<5 {
-                imgArr[i].image = UIImage(named: "grey_star")
-            }
-        }
-        
-    }
-
-}
+//class RateStarView: UIView {
+//    var imgArr: [UIImageView] = []
+//    var count = 3{
+//        didSet{
+//            refreshView()
+//        }
+//    }
+//
+//    override func drawRect(rect: CGRect) {
+//        super.drawRect(rect)
+//        refreshView()
+//    }
+//
+//    func refreshView() {
+//        let width = 17
+//        let height = 15
+//        if imgArr.count == 0 {
+//            for i in 0..<count{
+//                let imgView = UIImageView(image: UIImage(named: "red_star"))
+//              imgView.frame = CGRectMake(CGFloat(i*width), 0, CGFloat(width), CGFloat(height))
+//                self.imgArr.append(imgView)
+//                self.addSubview(imgView)
+//            }
+//            for i in count..<5 {
+//                let imgView = UIImageView(image: UIImage(named: "grey_star"))
+//                imgView.frame = CGRectMake(CGFloat(i*width), 0, CGFloat(width), CGFloat(height))
+//                self.imgArr.append(imgView)
+//                self.addSubview(imgView)
+//            }
+//        }
+//        else{
+//            for i in 0..<count{
+//                imgArr[i].image = UIImage(named: "red_star")
+//            }
+//            for i in count..<5 {
+//                imgArr[i].image = UIImage(named: "grey_star")
+//            }
+//        }
+//        
+//    }
+//
+//}
